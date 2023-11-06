9936dfca22105ff0d18467d63b4187082ebc20cb
iamsixwater <soup.sixwater@gmail.com>
2023-11-07 10:49:09 +1100

    [ADD] text component, todo component

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { ImageComponent } from './components/page/item/image.js';
+import { TextComponent } from './components/page/item/text.js';
+import { TodoComponent } from './components/page/item/todo.js';
 import { PageComponent } from './components/page/page.js';
 class App {
   private readonly page: PageComponent;
@@ -11,6 +13,12 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const text = new TextComponent('this is title', 'this is text');
+    text.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('this is todo', 'this is todo text');
+    todo.attachTo(appRoot, 'beforeend');
   }
 }
 
---
+++ src/components/page/item/text.ts
@@ -0,0 +1,20 @@
+import { BaseComponent } from '../../component.js';
+
+export class TextComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, body: string) {
+    super(`<section class="text">
+              <h2 class="text__title"></h2>
+              <p class="text__body"></p>
+          </section>`);
+
+    const titleElement = this.element.querySelector(
+      '.text__title'
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+
+    const bodyElement = this.element.querySelector(
+      '.text__body'
+    )! as HTMLParagraphElement;
+    bodyElement.textContent = body;
+  }
+}

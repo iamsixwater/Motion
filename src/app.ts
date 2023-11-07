8b364fb843034917c478ee01df75739546b147d6
iamsixwater <soup.sixwater@gmail.com>
2023-11-07 11:48:35 +1100

    [ADD] video component

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ImageComponent } from './components/page/item/image.js';
 import { TextComponent } from './components/page/item/text.js';
 import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
 class App {
   private readonly page: PageComponent;
@@ -13,6 +14,12 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent(
+      'this is title',
+      'https://www.youtube.com/watch?v=K3-jG52XwuQ&t=441'
+    );
+    video.attachTo(appRoot, 'beforeend');
 
     const text = new TextComponent('this is title', 'this is text');
     text.attachTo(appRoot, 'beforeend');
---
+++ src/components/page/item/video.ts
@@ -0,0 +1,37 @@
+import { BaseComponent } from '../../component.js';
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player">
+                <iframe class="video__iframe"></iframe>
+            </div>
+            <h3 class="video__title"></h3>
+        </section>`);
+
+    const iframeElement = this.element.querySelector(
+      '.video__iframe'
+    )! as HTMLIFrameElement;
+    iframeElement.src = this.convertToEmbeddedUrl(url);
+
+    const titleElement = this.element.querySelector(
+      '.video__title'
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+
+  private convertToEmbeddedUrl(url: string): string {
+    const regex =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-_]{11}))|(?:youtu.be\/([a-zA-Z0-9-_]{11})))/;
+    const matches = url.match(regex);
+    const videoId = matches ? matches[1] || matches[2] : undefined;
+
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
+  }
+}
+// https://www.youtube.com/watch?v=8pXX2kZNORA&t=441
+// https://www.youtube.com/embed/8pXX2kZNORA
+// https://www.youtube.com/embed/8pXX2_kZNORA

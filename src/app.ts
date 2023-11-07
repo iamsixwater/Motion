9a822eac52136b3ca7547e64a63673b3b6dc2a56
iamsixwater <soup.sixwater@gmail.com>
2023-11-08 09:47:08 +1100

    [ADD] dialog for getting data

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,97 @@
 import { Component } from './components/component.js';
+import { InputDialog } from './components/dialog/dialog.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
+import { TextSectionInput } from './components/dialog/input/text-input.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { TextComponent } from './components/page/item/text.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { Composable, PageComponent } from './components/page/page.js';
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from './components/page/page.js';
 class App {
   private readonly page: Component & Composable;
-  constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+  constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      'image title',
-      'https://picsum.photos/600/300'
-    );
-    this.page.addChild(image);
+    // https://picsum.photos/600/300
+    // https://www.youtube.com/watch?v=K3-jG52XwuQ&t=441
 
-    const video = new VideoComponent(
-      'this is title',
-      'https://www.youtube.com/watch?v=K3-jG52XwuQ&t=441'
-    );
-    this.page.addChild(video);
+    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+    imageBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new MediaSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
 
-    const text = new TextComponent('this is title', 'this is text');
-    this.page.addChild(text);
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
 
-    const todo = new TodoComponent('this is todo', 'this is todo text');
-    this.page.addChild(todo);
+      dialog.setOnSubmitListener(() => {
+        const image = new ImageComponent(inputSection.title, inputSection.url);
+        this.page.addChild(image);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
+    videoBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new MediaSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        const video = new VideoComponent(inputSection.title, inputSection.url);
+        this.page.addChild(video);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const textBtn = document.querySelector('#new-text')! as HTMLButtonElement;
+    textBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new TextSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        const text = new TextComponent(inputSection.title, inputSection.body);
+        this.page.addChild(text);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
+    todoBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const inputSection = new TextSectionInput();
+      dialog.addChild(inputSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(dialogRoot);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        const text = new TodoComponent(inputSection.title, inputSection.body);
+        this.page.addChild(text);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
   }
 }
 
-new App(document.querySelector('.document')! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement, document.body);
